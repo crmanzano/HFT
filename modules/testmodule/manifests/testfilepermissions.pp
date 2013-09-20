278e35f4c833c7d3c796b1c05b36dce120772038
rudymccomb <rudy@thnkbig.com>
2013-09-20 16:33:52 -0700

    added testfilepermissions.pp

---
+++ modules/testmodule/manifests/testfilepermissions.pp
@@ -0,0 +1,14 @@
+class testmodule::testfilepermissions{
+
+        #create home folder directory
+        file { '/Users/madmin/test':
+                ensure     => 'directory',
+        }
+
+	# Just verify file permissions.
+	file { "/Users/madmin/test/RestrictedFile.txt":
+		mode => 755,
+		ensure => present,
+		content => "Nobody read the contents of this file.",
+		}
+}
