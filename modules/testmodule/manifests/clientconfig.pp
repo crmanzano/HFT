347425ca7a35207a1b06280ef9977e4cc8a4d323
Rudy McComb <rudy@thnkbig.com>
2013-09-20 17:48:55 -0700

    Create clientconfig.pp
    enforces puppet.conf n puppet.plist on client

---
+++ modules/testmodule/manifests/clientconfig.pp
@@ -0,0 +1,20 @@
+class testmodule::config{
+	# Puts /etc/puppet/modules/testmodule/files/com.puppetlabs.puppet.plist on the client.
+	# On the client, it's going into /Library/LaunchDaemons/com.puppetlabs.puppet.plist
+	file { "/Library/LaunchDaemons/com.puppetlabs.puppet.plist":
+		source => "puppet:///modules/testmodule/files/com.puppetlabs.puppet.plist",
+		owner => root,
+		mode => 755,
+		ensure => present,
+		}
+		
+	# Puts /etc/puppet/modules/testmodule/puppet.conf on the client.
+	# On the client, it's going into /etc/puppet/puppet.conf
+	file { "/etc/puppet/puppet.conf":
+		source => "puppet:///modules/testmodule/files/puppet.conf",
+		owner => root,
+		mode => 755,
+		ensure => present,
+		}
+		
+}
