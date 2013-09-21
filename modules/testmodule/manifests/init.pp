42d480e98be662d0906a9e22b2fc5a2a3228cc27
Rudy McComb <rudy@thnkbig.com>
2013-09-20 17:50:52 -0700

    Update init.pp
    added clientconfig.pp

--- modules/testmodule/manifests/init.pp
+++ modules/testmodule/manifests/init.pp
@@ -3,6 +3,7 @@ class testmodule{
 include testmodule::config
 include testmodule::testfilepermissions
 include testmodule::adminusers
+include testmodule::clientconfig.pp
 #include testmodule::users
 #include testmodule::apps
 }
