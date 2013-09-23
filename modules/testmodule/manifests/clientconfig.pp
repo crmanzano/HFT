89d1b6b580f0a6b0f2b7964ff712b5fa8a747f32
rudymccomb <rudy@thnkbig.com>
2013-09-23 10:12:21 -0700

    'altered the path for source and added wheel for group'

--- modules/testmodule/manifests/clientconfig.pp
+++ modules/testmodule/manifests/clientconfig.pp
@@ -2,9 +2,10 @@ class testmodule::clientconfig{
 	# Puts /etc/puppet/modules/testmodule/files/com.puppetlabs.puppet.plist on the client.
 	# On the client, it's going into /Library/LaunchDaemons/com.puppetlabs.puppet.plist
 	file { "/Library/LaunchDaemons/com.puppetlabs.puppet.plist":
-		source => "puppet:///modules/testmodule/files/com.puppetlabs.puppet.plist",
+		source => "puppet:///modules/testmodule/com.puppetlabs.puppet.plist",
 		owner => root,
-		mode => 755,
+		group => wheel,
+		mode => 644,
 		ensure => present,
 		}
 		
--- modules/testmodule/manifests/init.pp
+++ modules/testmodule/manifests/init.pp
@@ -3,7 +3,7 @@ class testmodule{
 include testmodule::config
 include testmodule::testfilepermissions
 include testmodule::adminusers
-include testmodule::clientconfig.pp
+include testmodule::clientconfig
 #include testmodule::users
 #include testmodule::apps
 }
