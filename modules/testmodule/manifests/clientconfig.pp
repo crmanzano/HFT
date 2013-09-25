d17cc0ff28e847f22aaddc031dc598606b56f27e
rudymccomb <rudy@thnkbig.com>
2013-09-25 12:02:09 -0700

    migrated from static puppet.conf to puppet.conf.erb template

--- modules/testmodule/manifests/clientconfig.pp
+++ modules/testmodule/manifests/clientconfig.pp
@@ -11,13 +11,24 @@ class testmodule::clientconfig{
 		
 	# Puts /etc/puppet/modules/testmodule/puppet.conf on the client.
 	# On the client, it's going into /etc/puppet/puppet.conf
-	file { "/etc/puppet/puppet.conf":
-		source => "puppet:///modules/testmodule/puppet.conf",
-		owner => root,
-		group => wheel,
-		mode => 644,
-		ensure => present,
-		}
+	#file { "/etc/puppet/puppet.conf":
+	#	source => "puppet:///modules/testmodule/puppet.conf",
+	#	owner => root,
+	#	group => wheel,
+	#	mode => 644,
+	#	ensure => present,
+	#	}
+
+
+        # Puts /etc/puppet/modules/testmodule/puppet.conf on the client.
+        # On the client, it's going into /etc/puppet/puppet.conf
+        file { "/etc/puppet/puppet.conf":
+		content => template("testmodule/puppet.conf.erb"),
+                owner => root,
+                group => wheel,
+                mode => 644,
+                ensure => present,
+                }
 
 
 	#turns on puppetd n launchd
