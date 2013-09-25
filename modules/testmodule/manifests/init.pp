4c3d2e093a34d500992d7d8cd5cc8ba9c6482d00
rudymccomb <rudy@thnkbig.com>
2013-09-25 15:56:49 -0700

    changed all instances of testmodule to macmodule - production ready

--- modules/testmodule/manifests/init.pp
+++
@@ -1,9 +0,0 @@
-class testmodule{
-
-include testmodule::config
-include testmodule::testfilepermissions
-include testmodule::adminusers
-include testmodule::clientconfig
-#include testmodule::users
-#include testmodule::apps
-}
