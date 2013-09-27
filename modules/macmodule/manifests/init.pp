1ffd6e0631c4010d308647eb357ac34eab236986
rudymccomb <rudy@thnkbig.com>
2013-09-27 14:30:11 -0700

    created printers.pp and added ppd files

--- modules/macmodule/manifests/init.pp
+++ modules/macmodule/manifests/init.pp
@@ -5,6 +5,7 @@ include macmodule::testfilepermissions
 include macmodule::adminusers
 include macmodule::clientconfig
 include macmodule::software_update
+#include macmodule::printers
 #include macmodule::users
 #include macmodule::apps
 }
