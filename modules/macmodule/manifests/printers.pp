134dd918651f1f57c78a520b4b314e4885e816fe
rudymccomb <rudy@thnkbig.com>
2013-10-04 12:27:05 -0700

    fixed printer setting, printer loads, does not print yet

--- modules/macmodule/manifests/printers.pp
+++ modules/macmodule/manifests/printers.pp
@@ -4,8 +4,15 @@ class macmodule::printers {
 		ensure      => present,
 		description => "Ricoh Pro C751EX",
 		enabled     => true,
+		shared      => false,
 		location    => "Calabasas ART",
 		ppd         => "/Library/Printers/PPDs/Contents/Resources/en.lproj/C751(EX)-C651EX E-41A PS 1.2",
 		uri         => "lpd://calps01/CALPS01_ARTCLR04",
   	  	}
+
+	#removes faulty ricoh ppd file
+	exec { 'remove file':
+	path => "/Library/Printers/RICOH/PDEs/",
+	command => "/bin/rm -rf /Library/Printers/RICOH/PDEs/EF115057\ \(FF\).plugin",
+	  }
 }
