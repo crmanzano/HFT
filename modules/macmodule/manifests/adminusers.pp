0702b5a594ade31efc6d35751fb1a7cc8496d467
rudymccomb <rudy@thnkbig.com>
2013-10-07 20:22:13 -0700

    commented out managehome, osx n puppet cant do this

--- modules/macmodule/manifests/adminusers.pp
+++ modules/macmodule/manifests/adminusers.pp
@@ -13,11 +13,12 @@ class macmodule::adminusers {
 		comment => 'Mac Administrator', 
 		gid => '20', 
 		groups => ['_appserveradm', '_appserverusr', '_lpadmin', 'admin'], 			
-		home => '/Users/madmin', iterations => '38610', 
+		home => '/Users/madmin', 
+		iterations => '38610', 
 		password => '7b57a817fc9bca4c1bef2c5c8b8b3870a07adb6129757e374951bb70b51405e4aabffdd7ed1a5e9a3ef341b2fb53ca65db5708dae3202bfe83b67cfd34920eba3c1dbecf712b2b9d06ceb7e91b7324fab1cd2105fb0139af5e637ee1b6d6786881081efbbe388713c454656700afe5459efb14e4d9147f3219a42b6f5b48d88b', salt => 'cf76ea80e22d0f952a48303fb7067408cb8ffed79e6587c55cf48d25e8a0f26a', 
 		shell => '/bin/bash', 
 		uid => '501',
-		managehome => true, 
+		#managehome => true, 
 	}
   		
 
